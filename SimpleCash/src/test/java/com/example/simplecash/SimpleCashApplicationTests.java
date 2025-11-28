66967d2ffdfe8da6eb3e44a028638131e04981e8
Quentin Arbaut <quentin.arbaut@epita.fr>
2025-11-28 10:18:36 +0100

    Initialisation du projet

---
+++ SimpleCash/src/test/java/com/example/simplecash/SimpleCashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplecash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
